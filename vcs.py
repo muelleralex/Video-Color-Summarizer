8106dabb256da935720f451e6a24104d347e72bb
Alex Mueller <ajmueller@uchicago.edu>
2017-08-24 22:38:47 -0400

    fixed a few issues in the vcs.py

--- vcs.py
+++ vcs.py
@@ -1,3 +1,16 @@
+"""
+IDEAS:
+-   instead of shuffling pixels for sampling, just take every nth pixel, so
+    instead of sampling 30% of the pixels you could simply take every 10th pixel which would realistically give a better summary of the frame and also a more
+    consistent result each time
+-   to get every 10th pixel, just do array[0::10], not sure how I would do
+    something like 5% or something (I think every 10th pixel would be 10%)
+-   need to find a way to deal with missing frames and opencv errors
+-   need method for dealing with unfinished color summaries
+-   need to store frame number => convert to time in film
+"""
+
+
 from __future__ import division
 
 import cv2,random,argparse,json,math,sys
@@ -27,7 +40,7 @@ ap.add_argument("-v", "--video", required=True, help="Path to video", metavar="<
 ap.add_argument("-k", "--clusters", required=False, type=int, metavar="<clusters>", help="# of clusters", default=10)
 ap.add_argument("-f", "--frame", required=False, type=int, metavar="<frame>", help="Set granularity", default=60)
 ap.add_argument("-i", "--image", required=False, action="store_true", help="Display frames")
-ap.add_argument("-s", "--sample", required=False, type=restrict_float, metavar="<sample size>", help="Set fraction of sample size", default=0.5));
+ap.add_argument("-s", "--sample", required=False, type=restrict_float, metavar="<sample size>", help="Set fraction of sample size", default=0.1)
 args = vars(ap.parse_args())
 
 ## SET VARIABLES ##
@@ -49,8 +62,10 @@ else:
 k = args["clusters"]
 # number of frames to skip
 granularity = fps*args["frame"]
-# total pixels * sample = number of pixels to sample from frame
-sample = args["sample"]
+# total pixels in frame * sample = number of pixels to sample from frame
+sample_percent = args["sample"]
+# calculate number of pixels to sample per ten pixels
+sample = int(1/sample_percent)
 # whether to show frames while executing
 showImage = args["image"]
 # number of frames looked at
@@ -59,16 +74,23 @@ success = True
 # number of frames summarized
 bars = 0
 # head of JSON data
-data = [{'granularity' : granularity, 'clusters' : k}]
+data = [{'granularity' : granularity, 'clusters' : k, 'sampling' : sample_percent, 'fps' : fps}]
 
-## PREPARE VIDEO ##
-
+print "Frames Per Second:",fps
+print "Clusters:",k
+print "Sampling:",sample_percent
+print
 
 ## SUMMARIZE VIDEO ##
 while success:
     success,image = vidcap.read()
     # proceeed if success and frame is to be summarized
     if success and (count % granularity) == 0:
+        # calculate time of frame
+        seconds = count/fps
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        time = "%d:%02d:%02d" % (h,m,s)
         progress(count,totalframes,"Summarizing colors")
         # colors are stored backwards for some reason
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -84,13 +106,10 @@ while success:
         w, h, d = tuple(image.shape)
         # calculate total number of pixels
         totalPixels = w*h
-        # sample size
-        pixelSample = int(math.floor((w*h)/sample))
         # reshape image to one dimensional array
         image_array = np.reshape(image, (totalPixels, d))
-        # randomly shuffle elements of the array
-        # pick pixelSample number of points from the array
-        image_array_sample = shuffle(image_array, random_state=0)[:pixelSample]
+        # sample pixels
+        image_array_sample = image_array[0::sample]
         # run the k-means algorithm to find k colors on the sample pixels
         kmeans = KMeans(n_clusters=k, random_state=0).fit(image_array_sample)
         # label each pixel in image
@@ -98,7 +117,6 @@ while success:
         # convert labels into python list
         colorIndices = list(labels)
         colorCount = {}
-
         # for each cluster...
         for c in range(k):
             RGBvals = []
@@ -112,10 +130,8 @@ while success:
                 "G" : RGBvals[1],
                 "B" : RGBvals[2]
             }
-
         # add color information for frame to data
-        data.append(colorCount)
-        # print "bar:",bars
+        data.append({'colors' : colorCount, 'time' : time, 'frame' : count})
     # increment total number of frames passed over so far
     count += 1
 progress(1,1, "Finished summarizing colors!")
@@ -128,6 +144,6 @@ data[0]['bars'] = bars
 # create file name off of video title
 colorFile = videoFile.split('.mp4')[0] + '.json'
 # convert data to JSON file
-print "Packaging into JSON file"
+print "Packaging into JSON file..."
 with open(colorFile, 'w') as outfile:
     json.dump(data, outfile)
